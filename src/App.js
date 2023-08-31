bc9f64bab19f583de5db645506cb58903774ad07
remi <remi.nkjm@gmail.com>
2023-08-31 14:43:41 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="layout">
+      <header>HEADER</header>
+
+      <main>
+        <section id="content">
+          <h1>Big Title</h1>
+          <ul className="item-list">
+            <li>
+              <h2>Title 01</h2>
+              <div>
+                Proin ex nunc, bibendum ut magna quis.
+              </div>
+            </li>
+
+            <li>
+              <h2>Title 02</h2>
+              <div>
+                Blandit mollis orci. Ut pretium diam ut tristique interdum amet condimentum.
+              </div>
+            </li>
+
+            <li>
+              <h2>Title 03</h2>
+              <div>
+                Donec ut libero pretium, efficitur nisl vel, sagittis elit.
+              </div>
+            </li>
+          </ul>
+        </section>
+
+        <aside>
+          ASIDE
+        </aside>
+
+        <ul className="box-list">
+          <li>Box 1</li>
+          <li>Box 2</li>
+          <li>Box 3</li>
+          <li>Box 4</li>
+        </ul>
+      </main>
+
+      <footer>FOOTER</footer>
     </div>
   );
-}
+};
 
 export default App;
